adf3a146b37c89cacf363b8d3df4ee3aa1927211
alecristia <alecristia@gmail.com>
2021-02-09 16:22:58 +0100

    r pipeline with notes for real

--- 202010_jslhr/generate_jslhr_data.R
+++ 202010_jslhr/generate_jslhr_data.R
@@ -1,107 +1,167 @@
+## PHASE 1 -- RESTRICT ZOONIVERSE DATA TO PU DATA
+
 library(rjson)
-#file that has "subject" information -- in Zooniverse terms, that is info about the file that was classified
-zoosj <- read.csv("../data_analyses/output/metadata_all_PU.csv",header=T,sep =",")
+getname2=function(x) fromJSON(x)[[1]]
+
+#original chunk metadata
+origmetadata <- read.csv("../data_analyses/files_from_elsewhere/metadata_all_PU.csv",colClasses="factor",header=T,sep =",")
+dim(origmetadata)
+origmetadata$filename=paste0(origmetadata$AudioData,".mp3")
+length(levels(factor(origmetadata$filename)))
+#33730
+
+# #file that has "subject" information -- in Zooniverse terms, that is info about the file that was classified
+zoosj <- read.csv("../data_analyses/files_from_zooniverse/maturity-of-baby-sounds-subjects.csv",header=T,sep =",")
+zoosj$filename=unlist(lapply(as.character(zoosj[,"metadata"]), getname2))
+length(levels(factor(zoosj$filename)))
+#154063
+sum(levels(factor(zoosj$filename)) %in% levels(factor(origmetadata$filename)))
+#33730
+
+merge(origmetadata,zoosj,by="filename")->zoosj_PU
+dim(zoosj_PU)
+#33881 because some of the names are repeated
+mytab=table(zoosj_PU$filename)
+length(names(mytab[mytab==2]))
+#exploration revealed that one batch of 151 was uploaded twice -- removing the second upload
+for(mych in names(mytab[mytab==2])){
+  second_upload_id=max(zoosj_PU[zoosj_PU$filename==mych,"subject_id"])
+  zoosj_PU=zoosj_PU[zoosj_PU$subject_id!=second_upload_id,]
+}
+dim(zoosj_PU)
+#33730
+
+write.csv(zoosj_PU,"../data_analyses/output/zoo_subj_info_pu.csv")
+
+read.csv("../data_analyses/output/zoo_subj_info_pu.csv",colClasses="factor",header=T,sep =",")->zoosj
 
 #file that has the classifications (ie cit sci answers)
-classifs <- read.csv("../data_analyses/files_from_zooniverse/zooniverse_data_all_final.csv",header=T,sep =",")
+classifs <- read.csv("../data_analyses/files_from_zooniverse/maturity-of-baby-sounds-classifications.csv",header=T,sep =",")
+classifs$subject_id=fromJSON(as.character(classifs[,"subject_data"]))[[1]]$retired$subject_id
+length(levels(factor(classifs$subject_id)))
+#79162
+classifs -> full_classifs
+classifs=subset(classifs, classifs$subject_id %in% levels(factor(zoosj$subject_id)))
+length(levels(factor(classifs$subject_id)))
+#33730 chunks
 
-# <-- solution!! get json data which has classif info & match up using subject_id
+write.csv(classifs,"../data_analyses/output/zoo_anno_info_pu.csv")
 
+read.csv("../data_analyses/output/zoo_anno_info_pu.csv",colClasses="factor",header=T,sep =",")->classifs
 
-#children's info
-demo_data <- read.csv("../data_analyses/files_from_elsewhere/demo-data.tsv",header=T,sep ="\t")
+## PHASE 2 -- CLEAN CLASSIFICATIONS
 
-#dictionary that relates chunk to segment created by Chiara
-dict <- fromJSON(file="../data_analyses/files_from_elsewhere/dict_4.json") 
-dict.simple=unlist(dict)
-
-#laboratory annotations
-read.csv("../data_analyses/files_from_elsewhere/result_final_lisa.csv")->lab_jud
+library(rjson)
+getval=function(x) fromJSON(x)[[1]]$value
+getname=function(x) fromJSON(x)[[1]]$Name
 
 
 ## Cleaning stuff up ##
-# Remove weird answers, which do not belong here
-classifs$Answer<-factor(classifs$Answer,levels=c("Canonical","Non-Canonical","Crying","Laughing","Junk"))
-summary(classifs$Answer)
-classifs=classifs[!is.na(classifs$Answer),]
-classifs$AudioData=as.factor(classifs$AudioData)
-head(classifs)
+classifs$Answer=unlist(lapply(as.character(classifs[,"annotations"]), getval))
+table(classifs$Answer)
+
+classifs$filename=unlist(lapply(as.character(classifs[,"subject_data"]), getname))
+length(levels(factor(classifs$filename)))
 
 # Remove additional votes when there are more than 5, 
-# and remove clips with fewer than 5, which likely do not belong to this project
-table(classifs$AudioData)->sumvotes
+# and remove clips with fewer than 5
+table(classifs$filename)->sumvotes
 toomany=rownames(sumvotes)[sumvotes>5]
 length(toomany)
 justright=rownames(sumvotes)[sumvotes==5]
 length(justright)
 toofew=rownames(sumvotes)[sumvotes<5]
 length(toofew)
+#1 chunk didn't receive 5 votes
 
 classifs$nclasschunk=NA
-clean=classifs[classifs$AudioData %in% justright,] #start by adding the normal ones
+clean=classifs[classifs$filename %in% justright,] #start by adding the normal ones
 for(thischunk in toomany){
-  classifs$nclasschunk[classifs$AudioData==thischunk]<-1:sum(classifs$AudioData==thischunk)
-  clean=rbind(clean,classifs[classifs$AudioData==thischunk & classifs$nclasschunk<=5 & !is.na(classifs$nclasschunk),])
+  classifs$nclasschunk[classifs$filename==thischunk]<-1:sum(classifs$filename==thischunk)
+  clean=rbind(clean,classifs[classifs$filename==thischunk & classifs$nclasschunk<=5 & !is.na(classifs$nclasschunk),])
 }
 
 #check that the procedure worked
-clean$AudioData=as.factor(as.character(clean$AudioData))
-table(clean$AudioData)->sumvotes
+clean$filename=as.factor(as.character(clean$filename))
+table(clean$filename)->sumvotes
 sum(sumvotes>5)
-sum(sumvotes==5)
+sum(sumvotes==5) #33729 chunks
 sum(sumvotes<5)
 
-write.csv(clean,"../data_analyses/output/clean_classifications.csv")
-read.csv("../data_analyses/output/clean_classifications.csv")->clean
-classifs<-clean
+merge(zoosj,clean,by="filename")->chunks
 
-## Generate views on the data ##
-#combine subject info & classifications
-zoosj$AudioData=paste0(zoosj$AudioData,".mp3")
-merge(zoosj,classifs,by="AudioData")->chunks
+write.csv(chunks,"../data_analyses/output/clean_classifications.csv")
 
-# > length(levels(factor(zoosj$AudioData)))
-# [1] 19691
-# > length(levels(factor(chunks$AudioData)))
-# [1] 17714
-# --> we are missing 2k chunks
+## PHASE 3 -- Generate views on the data: majority judgment on the chunk
 
-#use child ID in the chunkID, for ease of processing later
-chunks$chunkID=paste(chunks$ChildID,chunks$onset,chunks$AudioData)
+#file that contains subject info & annotation info from PU, 
+#filtered down to have 5 classifications per subject ID (= chunk)
+read.csv("../data_analyses/output/clean_classifications.csv")->chunks
 
+#concatenate childID, filename, onset, chunk_pos in case we need them later
+chunks$chunk_info=paste(chunks$ChildID,chunks$filename,chunks$onset,chunks$chunk_pos)
 
 # next version collapses across chunks to get the nb of different classifications
-table(chunks$chunkID,chunks$Answer)->mytab
+table(chunks$chunk_info,chunks$Answer)->mytab
 sumvotes<- t(t(apply(mytab, 1, sum))) #get the total number of votes
 nvotes<- t(t(apply(mytab, 1, max))) #get the number of votes for the most common cat 
 mytab_mj <- mytab >= 3  #set as True the answer types that have more than 3 votes
 mymj <- t(t(apply(mytab_mj, 1, function(u) paste( names(which(u)), collapse=", " )))) #get the answer that has majority vote
 maj_jud=data.frame(cbind(row.names(mytab),unlist(mymj),nvotes,sumvotes)) #combine all info
-colnames(maj_jud)<-c("chunkID","Answer","nvotes","sumvotes")
+colnames(maj_jud)<-c("chunk_info","Answer","nvotes","sumvotes")
 
 #recover childID
-maj_jud$ChildID = gsub(" .*","",maj_jud$chunkID)
+maj_jud$ChildID = gsub(" .*","",maj_jud$chunk_info)
 
-#recover AudioData (name of the classified chunk)
-maj_jud$AudioData = gsub(".* ","",maj_jud$chunkID)
-maj_jud$AudioData = gsub(".mp3","",maj_jud$AudioData)
+#recover filename (name of the classified chunk)
+maj_jud$filename = gsub(".* ","",gsub(".mp3.*",".mp3",maj_jud$chunk_info))
+
 
 #check
 head(maj_jud)
 
+write.csv(maj_jud,"../data_analyses/output/chunks_maj_judgments.csv")
+
+
+## PHASE 4 -- Generate views on the data: majority judgment on the segment
+library(rjson)
+
+
+#dictionary that relates chunk to segment created by Chiara
+dict <- fromJSON(file="../data_analyses/files_from_elsewhere/dict_4.json") 
+dict.simple=unlist(dict)
+length(dict.simple) 
+dict=data.frame(cbind(dict.simple,gsub(".$","",as.character(names(dict.simple)))))
+colnames(dict)<-c("chunk","segmentId_DB")
+length(levels(factor(dict$chunk)))
+#33728 chunks
+length(levels(factor(dict$segmentId_DB)))
+#12170 segments
+#NOTE!!! THERE ARE TWO CHUNKS MISSING FROM THIS DICTIONARY!!!
+
+#laboratory annotations
+read.csv("../data_analyses/files_from_elsewhere/result_final_lisa.csv")->lab_jud
+dim(lab_jud)
+#11982 segments
+
+sum(lab_jud$segmentId_DB %in% levels(factor(dict$segmentId_DB))) #11980 in common
+sum(!(lab_jud$segmentId_DB %in% levels(factor(dict$segmentId_DB)))) #2 fund in lab but not dict
+sum(!(levels(factor(dict$segmentId_DB)) %in% lab_jud$segmentId_DB )) #190 found in dict but not in lab
+
+#assuming that we should follow lab data, I'll kick out any segment not in there
+
+#chunk info
+read.csv("../data_analyses/output/chunks_maj_judgments.csv",colClasses="factor")->maj_jud
+maj_jud$AudioData=gsub(".mp3","",maj_jud$filename)
+
+
 # next I need to match up the segments with the chunks
-maj_jud$segmentId_DB=NA
-sum(dict.simple %in% maj_jud$AudioData) # 17714 found
-sum(!(dict.simple %in% maj_jud$AudioData)) # 16014 not found
+sum(dict.simple %in% maj_jud$AudioData) # 33727 found
+sum(!(dict.simple %in% maj_jud$AudioData)) # 1 not found
 
-#<-- dict 11980 versus lab_jud has 11982 segmens
-# dict has 33728 chunks
-
-
-#limit to chunks for which we have data, to go faster
-for(thischunk in dict.simple[dict.simple %in% maj_jud$AudioData]) maj_jud$segmentId_DB[maj_jud$AudioData==thischunk]<-names(dict.simple[dict.simple==thischunk])
-maj_jud$segmentId_DB_old<-maj_jud$segmentId_DB
-maj_jud$segmentId_DB=gsub(".$","",maj_jud$segmentId_DB)
+rownames(dict)<-dict$chunk
+maj_jud$segmentId_DB <- dict[maj_jud$AudioData,"segmentId_DB"]
+length(levels(factor(maj_jud$segmentId_DB))) #12170
 
 #generate majority at the segment level using our rule:
 # canonical > non-canonical > crying > laughing > junk
@@ -117,12 +177,7 @@ zoo_jud=cbind(row.names(mytab),mytype)
 colnames(zoo_jud)<-c("segmentId_DB","Answer")
 
 merge(zoo_jud,lab_jud,by="segmentId_DB")->all_jud
-
-#write everything out
-merge(demo_data,chunks,by="ChildID")->chunks
-write.csv(chunks,"../data_analyses/output/chunks_individual_judgments.csv")
-
-merge(demo_data,maj_jud,by="ChildID")->maj_jud2
-write.csv(maj_jud2,"../data_analyses/output/chunks_maj_judgments.csv")
+#11980 segments
 
 write.csv(all_jud,"../data_analyses/output/zoo_lab_maj_judgments.csv")
+
